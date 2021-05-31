3b7055d39094c2adb08995cacc2766ca72d0fa65
SumanthNadig <psumanth6636@gmail.com>
2021-05-31 17:36:33 +0530

    Added method without division

--- Solutions/Problem2.py
+++ Solutions/Problem2.py
@@ -2,11 +2,25 @@ import array
 from operator import mul
 from functools import reduce
 
+# Using division
 arr = array.array('i', [1, 2, 3, 4, 5])
 result = list()
 prod = reduce(mul, arr.tolist(), 1)
 
 for i in arr:
     result.append(round(prod / i))
+
+print(result)
+
+# Another way without division
+arr = array.array('i', [1, 2, 3, 4, 5])
+x = list()
+result = list()
+prod = reduce(mul, arr.tolist(), 1)
+
+for i in arr:
+    x = arr.tolist()
+    x.pop(arr.index(i))
+    result.append(reduce(mul, x, 1))
 
 print(result)
